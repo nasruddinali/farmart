eeeb3386fb5b11c5ade13fac20909777f0f908bf
Nasruddin Ali <nasruddinali9246@gmail.com>
2023-08-18 11:35:24 +0530

    changing port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const router =express.Router()
 app.use(express.json())
 
 
-const port = process.env.PORT||3000;
+const port = 80;
 app.listen(port, () => {
   console.log(`listning to port ${port}`);
 });
